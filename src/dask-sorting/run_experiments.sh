ca5d1a3d13d2618cf8ab193ef4d269457f7b729b
Mark Coletti <colettima@ornl.gov>
2025-05-02 17:30:38 -0400

    Repaired master experiment script

--- src/dask-sorting/run_experiments.sh
+++ src/dask-sorting/run_experiments.sh
@@ -3,15 +3,21 @@
 # Serially run all the experiments
 
 # How many tasks to run
-n=1000
+n=3000
 
-python ./dask-sorting --output=descending_stealing.csv $n
-python ./dask-sorting --sorting=ascending --output=ascending_stealing.csv $n
-python ./dask-sorting --sorting=non --output=none_stealing.csv $n
+# do 30 iterations for each experimental level
+for i in {1..30}; do
+  echo "Iteration $i"
 
-python ./dask-sorting --stealing=False --output=descending_nostealing.csv $n
-python ./dask-sorting --stealing=False  --sorting=ascending --output=ascending_nostealing.csv $n
-python ./dask-sorting --stealing=False  --sorting=non --output=none_nostealing.csv $n
+  python dask-sorting.py --job $i --output=descending_stealing.csv $n
+  python dask-sorting.py --job $i --sorting=ascending --output=ascending_stealing.csv $n
+  python dask-sorting.py --job $i --sorting=none --output=none_stealing.csv $n
 
+  python dask-sorting.py --job $i --no-stealing --output=descending_nostealing.csv $n
+  python dask-sorting.py --job $i --no-stealing --sorting=ascending --output=ascending_nostealing.csv $n
+  python dask-sorting.py --job $i --no-stealing --sorting=none --output=none_nostealing.csv $n
 
+done
+
+echo "Done!"
 say "Your script has finished"
