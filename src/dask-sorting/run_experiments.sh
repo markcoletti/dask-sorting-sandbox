48bb8fe024c32694fa4f96c10e6538a258ab6738
Mark Coletti <colettima@ornl.gov>
2025-05-04 23:30:07 -0400

    Forgot to put the job ID in each file name to uniquely identify each CSV file by job

--- src/dask-sorting/run_experiments.sh
+++ src/dask-sorting/run_experiments.sh
@@ -6,16 +6,16 @@
 n=3000
 
 # do 30 iterations for each experimental level
-for i in {1..30}; do
+for i in {1..29}; do
   echo "Iteration $i"
 
-  python dask-sorting.py --job $i --output=descending_stealing.csv $n
-  python dask-sorting.py --job $i --sorting=ascending --output=ascending_stealing.csv $n
-  python dask-sorting.py --job $i --sorting=none --output=none_stealing.csv $n
+  python dask-sorting.py --job $i --output=descending_stealing_${i}.csv $n
+  python dask-sorting.py --job $i --sorting=ascending --output=ascending_stealing_${i}.csv $n
+  python dask-sorting.py --job $i --sorting=none --output=none_stealing_${i}.csv $n
 
-  python dask-sorting.py --job $i --no-stealing --output=descending_nostealing.csv $n
-  python dask-sorting.py --job $i --no-stealing --sorting=ascending --output=ascending_nostealing.csv $n
-  python dask-sorting.py --job $i --no-stealing --sorting=none --output=none_nostealing.csv $n
+  python dask-sorting.py --job $i --no-stealing --output=descending_nostealing_${i}.csv $n
+  python dask-sorting.py --job $i --no-stealing --sorting=ascending --output=ascending_nostealing_${i}.csv $n
+  python dask-sorting.py --job $i --no-stealing --sorting=none --output=none_nostealing_${i}.csv $n
 
 done
 
